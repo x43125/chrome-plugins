bda0f9f6ef3eca7d379d795c8ee72997655c55f0
x43125 <wx43125@163.com>
2025-02-17 22:41:37 +0800

    feat: update font color; +delete function

--- tab-label/popup.js
+++ tab-label/popup.js
@@ -1,6 +1,11 @@
 document.getElementById('labelForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const labelName = document.getElementById('labelName').value;
+    if (!labelName) {
+        alert('标签名不能为空');
+        return;
+    }
+
     const labelColor = document.getElementById('labelColor').value;
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -25,7 +30,19 @@ document.getElementById('labelForm').addEventListener('submit', function (event)
     });
 });
 
-
-document.getElementById('labelForm').addEventListener('delete', function (event) {
-    console.log('delete', event);
+document.getElementById('deleteLabel').addEventListener('click', function (event) {
+    // 获取当前标签的URL
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const currentTab = tabs[0];
+        const currentUrl = currentTab.url;
+        const parsedUrl = new URL(currentUrl);
+        const storageKey = `${parsedUrl.hostname}${parsedUrl.pathname}`;
+        // 移除
+        chrome.storage.sync.remove([storageKey], function () {
+            // 发送消息给 content.js 以刷新标签
+            chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { action: "refreshLabel" });
+            });
+        });
+    })
 });
